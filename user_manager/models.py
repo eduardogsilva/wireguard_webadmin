3177eb2b8d0ec18ff682b247057415356fff5d24
Eduardo Silva <eduardo@eth0.com.br>
2024-02-15 10:57:59 -0300

    User and permission management

--- user_manager/models.py
+++ user_manager/models.py
@@ -1,3 +1,18 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
+
+
+class UserAcl(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    user_level = models.PositiveIntegerField(default=0, choices=(
+        (10, 'Debugging Analyst'),
+        (20, 'View Only User'),
+        (30, 'Peer Manager'),
+        (40, 'Wireguard Manager'),
+        (50, 'Administrator'),
+
+    ))
+
+    def __str__(self):
+        return self.user.username
--- wireguard_webadmin/settings.py
+++ wireguard_webadmin/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'wireguard',
+    'user_manager',
 ]
 
 MIDDLEWARE = [
