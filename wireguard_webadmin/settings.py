013f12075624fb02a3acdaf0bece104a9272a015
Eduardo Silva <eduardo@eth0.com.br>
2024-03-08 12:32:05 -0300

    Mask for private key on Peer and Wireguard instance forms.

--- wireguard_webadmin/settings.py
+++ wireguard_webadmin/settings.py
@@ -129,6 +129,6 @@ STATICFILES_DIRS = [
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-WIREGUARD_WEBADMIN_VERSION = 9506
+WIREGUARD_WEBADMIN_VERSION = 9507
 
 from wireguard_webadmin.production_settings import *
