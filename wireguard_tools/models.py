b7ae6f142133ad87cb10800b3123c39c1ab282d5
Eduardo Silva <eduardo@eth0.com.br>
2025-02-25 09:21:50 -0300

    Add EmailSettings model with migration

--- wireguard_tools/models.py
+++ wireguard_tools/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+import uuid
 
-# Create your models here.
+
+class EmailSettings(models.Model):
+    name = models.CharField(default='email_settings', max_length=20, unique=True)
+    smtp_username = models.CharField(max_length=100, blank=True, null=True)
+    smtp_password = models.CharField(max_length=100, blank=True, null=True)
+    smtp_host = models.CharField(max_length=100, blank=True, null=True)
+    smtp_port = models.IntegerField(default=587)
+    smtp_encryption = models.CharField(default='tls', choices=(('ssl', 'SSL'), ('tls', 'TLS')), max_length=3)
+    smtp_from_address = models.EmailField(blank=True, null=True)
+
+    uuid = models.UUIDField(default=uuid.uuid4, editable=False, unique=True)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
