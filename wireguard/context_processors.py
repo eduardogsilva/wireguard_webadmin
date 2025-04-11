701f95764272dbb47f475fb9ed42d6c5e1a62bd3
Eduardo Silva <eduardo@eth0.com.br>
2025-04-11 11:16:50 -0300

    Check if user is authenticated before loading user_acl

--- wireguard/context_processors.py
+++ wireguard/context_processors.py
@@ -3,9 +3,10 @@ from .models import WireGuardInstance
 
 
 def pending_changes_warning(request):
-    user_acl = UserAcl.objects.filter(user=request.user).first()
     if request.user.is_authenticated:
+        user_acl = UserAcl.objects.filter(user=request.user).first()
         pending = WireGuardInstance.objects.filter(pending_changes=True).exists()
     else:
+        user_acl = None
         pending = False
     return {'pending_changes_warning': pending, 'user_acl': user_acl}
