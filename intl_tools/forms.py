6285ae6e68c00b8618543b80d6738cc72608fc7e
Eduardo Silva <eduardo@eth0.com.br>
2025-04-17 12:45:48 -0300

    Add informational message for translation issues in language change form

--- intl_tools/forms.py
+++ intl_tools/forms.py
@@ -1,5 +1,5 @@
 from crispy_forms.helper import FormHelper
-from crispy_forms.layout import Column, Layout, Row, Submit
+from crispy_forms.layout import Column, HTML, Layout, Row, Submit
 from django import forms
 from django.conf import settings
 from django.utils.translation import gettext_lazy as _
@@ -19,5 +19,13 @@ class LanguageForm(forms.Form):
             Row(
                 Column('language', css_class='col-md-6'),
             ),
-            Submit('submit', _("Change Language"), css_class='btn btn-primary')
+
+
+            Submit('submit', _("Change Language"), css_class='btn btn-primary'),
+
+            Row(
+            Column(HTML(
+                "<span class='small'>" + _("If you find any issues with the translation or would like to request a new language, please open an") + " <a href='https://github.com/eduardogsilva/wireguard_webadmin/issues' target='_blank'>issue</a>.</span>"),
+                   css_class='col-md-12', style='padding-top: 32px;'),
+        ),
         )
