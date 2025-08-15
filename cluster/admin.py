96eaa39f8d68effd4a43994a2510dff22460c377
Eduardo Silva <eduardo@eth0.com.br>
2025-08-14 21:44:52 -0300

    Register ClusterSettings, Worker, and WorkerStatus models in Django admin

--- cluster/admin.py
+++ cluster/admin.py
@@ -1,3 +1,22 @@
+from django.contrib import admin
+
+from .models import ClusterSettings, Worker, WorkerStatus
 
 
+@admin.register(ClusterSettings)
+class ClusterSettingsAdmin(admin.ModelAdmin):
+    list_display = ('name', 'enabled', 'cluster_mode', 'restart_mode', 'config_version', 'created', 'updated')
+    list_filter = ('enabled', 'cluster_mode', 'restart_mode', 'primary_enable_wireguard')
+
+
+@admin.register(Worker)
+class WorkerAdmin(admin.ModelAdmin):
+    list_display = ('name', 'enabled', 'ip_address', 'country', 'city', 'ip_lock', 'created', 'updated')
+    list_filter = ('enabled', 'ip_lock', 'country', 'force_reload', 'force_restart')
+
+
+@admin.register(WorkerStatus)
+class WorkerStatusAdmin(admin.ModelAdmin):
+    list_display = ('worker', 'last_seen', 'config_version', 'config_pending', 'last_reload', 'last_restart')
+    list_filter = ('config_pending', 'last_seen', 'last_reload', 'last_restart')
 
