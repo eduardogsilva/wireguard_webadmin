82a3f5403c86bed7de6fa55f63d9f86c400adaf4
Eduardo Silva <eduardo@eth0.com.br>
2025-12-29 18:30:39 -0300

    update translations

--- update_messages.sh
+++ update_messages.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-django-admin makemessages -a --ignore=.venv/*
-django-admin compilemessages --ignore=.venv/*
+.venv/bin/django-admin makemessages -a --ignore=.venv/*
+.venv/bin/django-admin compilemessages --ignore=.venv/*
