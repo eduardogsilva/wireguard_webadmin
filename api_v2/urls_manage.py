f2dc9942d91d87e59e7a864adc1444fc283654a8
Eduardo Silva <eduardo@eth0.com.br>
2026-02-11 16:49:32 -0300

    Add API endpoints for listing peers and retrieving peer details

--- api_v2/urls_manage.py
+++ api_v2/urls_manage.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from api_v2.views import view_api_key_list, view_manage_api_key, view_delete_api_key
+from api_v2.views import view_api_key_list, view_manage_api_key, view_delete_api_key, view_api_docs
 
 urlpatterns = [
     path('list/', view_api_key_list, name='api_v2_list'),
     path('manage/', view_manage_api_key, name='api_v2_manage'),
     path('delete/<uuid:uuid>/', view_delete_api_key, name='api_v2_delete'),
+    path('docs/', view_api_docs, name='api_v2_docs'),
 ]
