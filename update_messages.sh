0e8f9e48b97328e747d2d4f3ffd2eb23579c1b78
Eduardo Silva <eduardo@eth0.com.br>
2026-02-12 13:18:19 -0300

    add language statistics output for .po files

--- update_messages.sh
+++ update_messages.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
 .venv/bin/django-admin makemessages -a --ignore=.venv/*
 .venv/bin/django-admin compilemessages --ignore=.venv/*
+echo
+for po in locale/*/LC_MESSAGES/django.po; do
+    lang=${po#locale/}
+    lang=${lang%%/*}
+    echo -n "$lang: "
+    msgfmt --statistics "$po" -o /dev/null 2>&1
+done
+
